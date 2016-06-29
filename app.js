0e9e8f77695a3902a578b8c0128603ff2e92a468
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-06-29 15:44:02 -0500

    working shuffle function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $('document').ready(function(){
 
-var names = [Richard, Jon, Tracy, Sahasha, BrianA, BrianV, Megan, Justin,
-Hillary, Liz, Donovan, Connor, Cormac, Ryan, Kyle, Andrew, Trent,
-Anthony, Katie, RyanM, Joel];
+var names = ['Richard', 'Jon', 'Tracy', 'Sahasha', 'BrianA', 'BrianV',
+ 'Megan', 'Justin', 'Hillary', 'Liz', 'Donovan', 'Connor', 'Cormac',
+ 'Ryan', 'Kyle', 'Andrew', 'Trent', 'Anthony', 'Katie', 'RyanM', 'Joel'];
 
 var groups = ['Star Wars Geeks', 'Harry Potter People', 'Team GoT',
 'Big Bang Theorists', 'Baseball Fans', 'Comedy Lovers', 'Brown Coats',
