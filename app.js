e13b56673b05b9ceb21552a7b45410517301ea36
jonwilsoncomedy <jonwilsoncomedy@gmail.com>
2016-06-30 10:42:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 $('document').ready(function(){
 
-
-var names = ['Richard', 'Jon', 'Tracy', 'Sahasha', 'BrianA', 'BrianV', 'Megan', 'Justin',
-'Hillary', 'Liz', 'Donovan', 'Connor', 'Cormac', 'Ryan', 'Kyle', 'Andrew', 'Trent',
-'Anthony', 'Katie', 'RyanM', 'Joel'];
+var names = ['Richard', 'Jon', 'Tracy', 'Sahasha', 'BrianA', 'BrianV',
+ 'Megan', 'Justin', 'Hillary', 'Liz', 'Donovan', 'Connor', 'Cormac',
+ 'Ryan', 'Kyle', 'Andrew', 'Trent', 'Anthony', 'Katie', 'RyanM', 'Joel'];
 
 var groups = ['Star Wars Geeks', 'Harry Potter People', 'Team GoT',
 'Big Bang Theorists', 'Baseball Fans', 'Comedy Lovers', 'Brown Coats',
 'Team Artsy', 'Ren Festie'];
+
+var numGroups = 0;
+
+// for() {
+// var starWarsGeeks = [];
+// var harryPotterPeople = [];
+// var teamGot = [];
+// var renFesties = [];
+// var bigBang = [];
+// var baseballFans = [];
+// var comedyLovers = [];
+// var brownCoats = [];
+// var teamArtsy = [];
+// };
 
 
 function shuffle(array) {
@@ -28,13 +41,17 @@ function shuffle(array) {
 
   return array;
 }
-// Used like so
-// var arr = [2, 11, 37, 42];
-// shuffle(arr);
-// console.log(arr);
 
-shuffle(names);
-console.log(names);
+// shuffle(names);
+// console.log(names);
+
+$('body')on.('click', '#number' function() {
+  numGroups = parseInt($(this).val());
+
+
+$('body')on.('click', '.generator' function() {
+  shuffle(names);
+}
 
 
 
